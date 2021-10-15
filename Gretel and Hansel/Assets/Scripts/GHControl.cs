a0a1b2df75b7b3555c8c3e7411d96181d06bd831
Dianhanluosi <evanzheng1127@gmail.com>
2021-10-15 13:57:36 -0400

    .

--- Gretel and Hansel/Assets/Scripts/GHControl.cs
+++ Gretel and Hansel/Assets/Scripts/GHControl.cs
@@ -4,6 +4,36 @@ using UnityEngine;
 
 public class GHControl : MonoBehaviour
 {
+
+    //strings to reference inputs
+    public string playerNum;
+
+    public string H;
+
+    public string V;
+
+    public string A;
+
+    public string B;
+
+
+    //referencing rigidbody
+    Rigidbody2D rb;
+
+    //speed variables
+    public float xSpeed;
+
+    private void Awake()
+    {
+        H = "Horizontal" + playerNum ;
+        V = "Vertical" + playerNum;
+        A = "A" + playerNum;
+        B = "B" + playerNum;
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +43,10 @@ public class GHControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetAxis(H) != 0)
+        {
+            rb.AddForce(new Vector2(Input.GetAxis(H) * xSpeed * Time.deltaTime, 0));
+        }
+
     }
 }
