788b91309d548ae669f1fe25196c1668bb6f2094
Emily Xie <Xiee178@newschool.edu>
2021-10-18 16:08:40 -0400

    ?
    ??

---
+++ Audio demo/Assets/Audio.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    public AudioClip[] clips;
+    private float fadeSpeed = 1;
+    private AudioSource[] audioSources;
+    private indexer trackIndex = 0;
+    void Awake()
+    {
+        if(FindobjectOfType<BGMusic>().Length > 1)
+        {
+            Destory(gameObject);//to only exist in one copy
+        }
+        else
+        {
+            DontDestoryOnLoad(gameObject);
+            audioSources = GetComponent<AudioSource>();
+        }
+
+        private coid OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            StopAllCoroutine();
+            //whatever is fading in before, fades out now
+            StartCoroutine(FadeMusic(audioSources[trackIndex],audioSources[trackIndex = 1]- trackIndex], scene.buildIndex));
+           //changing the order of the sound track in thee scene matters
+        }
+        private IEnumerator FadeMusic( AudioSource fadeIn, AudioSource fadeout, int )
+    {
+        fadeIn.clip = clips[buildIndex];
+        fadeIn.Play();
+        float t = 0;
+        while (t < 1)
+        {
+            fadeIn.volume = Mathf.Smooth
+        }
+    }
+
+
+
+        
+
+        void OnEnable()
+        {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+
+        void OnDisable()
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+    }
+}
---
+++ Audio demo/Assets/Door.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnTriggerEner(Collider other)
+    {
+        if (other..ComparedTag("Player"))
+        {
+            //it will load the scene when you tell it to load
+
+
+            asyncOperation = SceneManager.LoadSceneAsync(levelName);
+            asyncOperation.allowSceneActivation = false;
+            fader.CrossFadeAlpha(1, 1, true);
+            StartCoroutine(LoadNext());
+        }
+    }
+}
